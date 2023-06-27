dae202382d65602b001a3d77e4e2651ddbaedcb7
Josue Martinez <aero.josue@gmail.com>
2023-06-27 19:46:36 -0300

    Position Class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,14 @@
 package application;
 
+import boardGame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World!");
+		Position pos = new Position(1, 3);
+		
+		System.out.println(pos);
 
 	}
 
